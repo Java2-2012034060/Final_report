25beac394d5f4e2377312cc932654cee6700439d
Java2-2012034081 <pjs5617@naver.com>
2016-06-20 01:38:12 +0900

    6/20 hotfix
    새벽 1시 38분

--- src/cal/Calulator.java
+++ src/cal/Calulator.java
@@ -1,24 +1,53 @@
 package cal;
+import java.awt.event.ActionListener;
 
-public class Calulator {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+ 
+ 
+ 
+ 
+public class Calulator extends JFrame implements ActionListener {
+ 
+	private JPanel panel;
+	private JTextField display;
+	private JButton[] buttons;
+	private String[] labels = { " ", " ", " ", " ", "C", "7", "8", "9",
+			"/", " ", "4", "5", "6", "x", " ", "1", "2", "3", "-", " ",
+			"0", " ", " ", "+", "=", };
+ 
+	private double result = 0;
+	private String operator = "=";
+	private boolean startOfNumber = true;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("ffff");
-		System.out.println("hi 장석");
-
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-
-		System.out.println("hi moon");
-		System.out.println("hi moon");
-		System.out.println("hi moon");
-		
-
+}
+public void actionPerformed(ActionEvent e) {
+	String command = e.getActionCommand();
+	if (command.charAt(0) == 'C') {
+		startOfNumber = true;
+		result = 0;
+		operator = "=";
+		display.setText("0.0");
+	} else if (command.charAt(0) >= '0' && command.charAt(0) <= '9'){
+		if (startOfNumber == true)
+			display.setText(command);
+		else
+			display.setText(display.getText() + command);
+		startOfNumber = false;
+	} else {
+		if (startOfNumber) {
+			if (command.equals("-")) {
+				display.setText(command);
+				startOfNumber = false;
+			} else
+				operator = command;
+		} else {
+			double x = Double.parseDouble(display.getText());
+			calculate(x);
+			operator = command;
+			startOfNumber = true;
+		}
 	}
-
 }
