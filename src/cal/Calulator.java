b38c26bb8f180ed892162958294c16d9c42fb2de
Java2-2012034081 <pjs5617@naver.com>
2016-06-20 01:34:18 +0900

    sd
    sdwd

--- src/cal/Calulator.java
+++ src/cal/Calulator.java
@@ -6,16 +6,19 @@ public class Calulator {
 		// TODO Auto-generated method stub
 		System.out.println("ffff");
 		System.out.println("hi 장석");
-<<<<<<< HEAD
+
 		System.out.println("hi 장석");
 		System.out.println("hi 장석");
 		System.out.println("hi 장석");
 		System.out.println("hi 장석");
 		System.out.println("hi 장석");
 		System.out.println("hi 장석");
-=======
+
 		System.out.println("hi moon");
->>>>>>> refs/remotes/origin/master
+		System.out.println("hi moon");
+		System.out.println("hi moon");
+		
+
 	}
 
 }
