7163ff547a1da7c0060ab79afbdaad08a3af1aff
Java2-2012034060 <gms00050@naver.com>
2016-06-20 01:57:05 +0900

    완성
    계산기 완성 후 테스트완료.

--- src/cal/Calulator.java
+++ src/cal/Calulator.java
@@ -1,112 +1,91 @@
-<<<<<<< HEAD
 package cal;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-<<<<<<< HEAD
-public class Calulator {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("ffff");
-		System.out.println("hi 장석");
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/Hotfix
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-		System.out.println("hi 장석");
-<<<<<<< HEAD
-=======
-
-		System.out.println("hi moon");
-		System.out.println("hi moon");
-		System.out.println("hi moon");
-		
-
->>>>>>> origin/Hotfix
+ 
+import javax.swing.*;
+ 
+ 
+ 
+ 
+public class Calulator extends JFrame implements ActionListener {
+ 
+	private JPanel panel;
+	private JTextField display;
+	private JButton[] buttons;
+	private String[] labels = { " ", " ", " ", " ", "C", "7", "8", "9",
+			"/", " ", "4", "5", "6", "x", " ", "1", "2", "3", "-", " ",
+			"0", " ", " ", "+", "=", };
+ 
+	private double result = 0;
+	private String operator = "=";
+	private boolean startOfNumber = true;
+ 
+	public Calulator() {
+		display = new JTextField(35);
+		panel = new JPanel();
+		display.setText("0");
+		panel.setLayout(new GridLayout(0, 5, 3, 3));
+		buttons = new JButton[25];
+		int index = 0;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				buttons[index] = new JButton(labels[index]);
+				panel.add(buttons[index]);
+				buttons[index].addActionListener(this);
+				index++;
+			}
+		}
+		add(display, BorderLayout.NORTH);
+		add(panel, BorderLayout.CENTER);
+		setVisible(true);
+		pack();
 	}
-
-}
-=======
-package cal;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
  
- 
- 
- 
-public class Calulator extends JFrame implements ActionListener {
- 
-	private JPanel panel;
-	private JTextField display;
-	private JButton[] buttons;
-	private String[] labels = { " ", " ", " ", " ", "C", "7", "8", "9",
-			"/", " ", "4", "5", "6", "x", " ", "1", "2", "3", "-", " ",
-			"0", " ", " ", "+", "=", };
- 
-	private double result = 0;
-	private String operator = "=";
-	private boolean startOfNumber = true;
-
-}
->>>>>>> refs/remotes/origin/master
-=======
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
- 
- 
- 
- 
-public class Calulator extends JFrame implements ActionListener {
- 
-	private JPanel panel;
-	private JTextField display;
-	private JButton[] buttons;
-	private String[] labels = { " ", " ", " ", " ", "C", "7", "8", "9",
-			"/", " ", "4", "5", "6", "x", " ", "1", "2", "3", "-", " ",
-			"0", " ", " ", "+", "=", };
- 
-	private double result = 0;
-	private String operator = "=";
-	private boolean startOfNumber = true;
-
-}
-public void actionPerformed(ActionEvent e) {
-	String command = e.getActionCommand();
-	if (command.charAt(0) == 'C') {
-		startOfNumber = true;
-		result = 0;
-		operator = "=";
-		display.setText("0.0");
-	} else if (command.charAt(0) >= '0' && command.charAt(0) <= '9'){
-		if (startOfNumber == true)
-			display.setText(command);
-		else
-			display.setText(display.getText() + command);
-		startOfNumber = false;
-	} else {
-		if (startOfNumber) {
-			if (command.equals("-")) {
-				display.setText(command);
-				startOfNumber = false;
-			} else
-				operator = command;
-		} else {
-			double x = Double.parseDouble(display.getText());
-			calculate(x);
-			operator = command;
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		if (command.charAt(0) == 'C') {
 			startOfNumber = true;
+			result = 0;
+			operator = "=";
+			display.setText("0.0");
+		} else if (command.charAt(0) >= '0' && command.charAt(0) <= '9'){
+			if (startOfNumber == true)
+				display.setText(command);
+			else
+				display.setText(display.getText() + command);
+			startOfNumber = false;
+		} else {
+			if (startOfNumber) {
+				if (command.equals("-")) {
+					display.setText(command);
+					startOfNumber = false;
+				} else
+					operator = command;
+			} else {
+				double x = Double.parseDouble(display.getText());
+				calculate(x);
+				operator = command;
+				startOfNumber = true;
+			}
 		}
 	}
+ 
+	private void calculate(double n) {
+		if (operator.equals("+"))
+			result += n;
+		else if (operator.equals("-"))
+			result -= n;
+		else if (operator.equals("x"))
+			result *= n;
+		else if (operator.equals("/"))
+			result /= n;
+		else if (operator.equals("="))
+			result = n;
+		display.setText("" + result);
+	}
+ 
+	public static void main(String args[]) {
+		Calulator calculator = new Calulator();
+	}
 }
->>>>>>> origin/Hotfix
